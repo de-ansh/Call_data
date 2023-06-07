0735129121b6eb5044245edb43f914fd04d68174
Ashish Mishra <anshm9448@gmail.com>
2023-06-07 19:56:46 +0530

    save done

--- lib/interface/socket.dart
+++ lib/interface/socket.dart
@@ -1,3 +1,4 @@
+import 'package:arquella_hub/view/add_call.dart';
 import 'package:flutter/material.dart';
 import 'package:arquella_hub/server/websocket_server.dart';
 import 'package:arquella_hub/data/database/database_screen.dart';
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'WebSocket Server',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -81,7 +83,12 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const AddCallPage()),
+            );
+          },
           tooltip: 'Add Call',
           child: const Icon(Icons.add),
         ));
--- lib/view/add_call.dart
+++ lib/view/add_call.dart
@@ -21,7 +21,34 @@ class _AddCallPageState extends State<AddCallPage> {
       TextEditingController();
   final TextEditingController _callStartDateController =
       TextEditingController();
+  final TextEditingController cll_db_date_id_Controller =
+      TextEditingController();
+  final TextEditingController cll_db_time_id_Controller =
+      TextEditingController();
+  final TextEditingController cll_call_group_ID_controller =
+      TextEditingController();
+
   final TextEditingController _callendDateController = TextEditingController();
+  final TextEditingController cll_worked_through_Controller =
+      TextEditingController();
+  final TextEditingController cll_carer_Controller = TextEditingController();
+  final TextEditingController cll_call_tracking_ref_controller =
+      TextEditingController();
+  final TextEditingController cll_clear_controller = TextEditingController();
+  final TextEditingController cll_fb_record_id_Controller =
+      TextEditingController();
+  final TextEditingController cll_panel_name_Controller =
+      TextEditingController();
+  final TextEditingController cll_journey_ref_controller =
+      TextEditingController();
+  final TextEditingController cll_caregroup_controller =
+      TextEditingController();
+  final TextEditingController cll_carehome_controller = TextEditingController();
+  final TextEditingController careHome_id_Controller = TextEditingController();
+  final TextEditingController created_at_controller = TextEditingController();
+  final TextEditingController updated_at_controller = TextEditingController();
+  DateTime? created_at;
+  DateTime? updated_at;
   DateTime? _callstrtDate;
   DateTime? _callendDate;
   @override
@@ -38,39 +65,198 @@ class _AddCallPageState extends State<AddCallPage> {
           children: [
             CustomTextFormField(
                 controller: _siteIdController, txtLabel: 'site Id'),
+            const SizedBox(
+              height: 15,
+            ),
             CustomDatePicker(
                 controller: _callStartDateController,
                 txtLabel: 'Call Start Date',
                 callback: () {
                   pickcallStartDate(context);
                 }),
+            const SizedBox(
+              height: 15,
+            ),
             CustomDatePicker(
                 controller: _callendDateController,
                 txtLabel: 'Birth Date',
                 callback: () {
                   pickCallEndDate(context);
                 }),
+            const SizedBox(
+              height: 15,
+            ),
             CustomTextFormField(
               controller: _callDurationController,
               txtLabel: 'Call Duration',
             ),
-            const SizedBox(height: 8),
+            const SizedBox(height: 15),
             CustomTextFormField(
                 controller: _cll_roomController, txtLabel: 'Calll Room'),
-            const SizedBox(height: 8),
+            const SizedBox(height: 15),
             CustomTextFormField(
                 controller: cll_zoneController, txtLabel: 'Call zone'),
+            const SizedBox(
+              height: 15,
+            ),
             CustomTextFormField(
                 controller: cll_typeController, txtLabel: 'Call typpe'),
+            const SizedBox(
+              height: 15,
+            ),
             CustomTextFormField(
                 controller: _cll_unit_ID_Controller, txtLabel: 'Call unit ID'),
-            const SizedBox(height: 8),
+            const SizedBox(
+              height: 15,
+            ),
             CustomTextFormField(
-                controller: cll_db_ref_id_controller, txtLabel: "Call Db  Ref")
+                controller: cll_db_ref_id_controller, txtLabel: "Call Db  Ref"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: cll_db_date_id_Controller,
+                txtLabel: "cll_db_date_id "),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: cll_db_time_id_Controller,
+                txtLabel: "cll_db_time_id"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: cll_call_group_ID_controller,
+                txtLabel: "cll_call_group_ID"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: cll_worked_through_Controller,
+                txtLabel: "cll_worked_through"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: cll_carer_Controller, txtLabel: "cll_carer"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: cll_call_tracking_ref_controller,
+                txtLabel: "cll_call_tracking_ref"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: cll_clear_controller, txtLabel: "cll_clear"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: cll_fb_record_id_Controller,
+                txtLabel: "cll_fb_record_id"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: cll_panel_name_Controller,
+                txtLabel: "cll_panel_name"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: cll_journey_ref_controller,
+                txtLabel: "cll_journey_ref"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: cll_caregroup_controller,
+                txtLabel: "cll_caregroup"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: cll_carehome_controller, txtLabel: "cll_careHome"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFormField(
+                controller: careHome_id_Controller, txtLabel: "careHome_id"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomDatePicker(
+                controller: updated_at_controller,
+                txtLabel: "updated_at",
+                callback: () {
+                  pickupdatedAtDate(context);
+                }),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomDatePicker(
+                controller: created_at_controller,
+                txtLabel: "createdAt",
+                callback: () {
+                  pickcreatedAtDate(context);
+                })
           ],
         ),
       ),
     );
+  }
+
+  Future<void> pickcreatedAtDate(BuildContext context) async {
+    final initialDate = DateTime.now();
+    final newDate = await showDatePicker(
+        context: context,
+        initialDate: _callstrtDate ?? initialDate,
+        firstDate: DateTime(DateTime.now().year - 100),
+        lastDate: DateTime(DateTime.now().year + 1),
+        builder: (context, child) => Theme(
+              data: ThemeData().copyWith(
+                  colorScheme: const ColorScheme.light(
+                      primary: Colors.pink,
+                      onPrimary: Colors.white,
+                      onSurface: Colors.black)),
+              child: child ?? const Text(''),
+            ));
+    if (newDate == null) {
+      return;
+    }
+    setState(() {
+      _callstrtDate = newDate;
+      String cllsrtdate = DateFormat('dd/MM/yyyy').format(newDate);
+      _callStartDateController.text = cllsrtdate;
+    });
+  }
+
+  Future<void> pickupdatedAtDate(BuildContext context) async {
+    final initialDate = DateTime.now();
+    final newDate = await showDatePicker(
+        context: context,
+        initialDate: _callstrtDate ?? initialDate,
+        firstDate: DateTime(DateTime.now().year - 100),
+        lastDate: DateTime(DateTime.now().year + 1),
+        builder: (context, child) => Theme(
+              data: ThemeData().copyWith(
+                  colorScheme: const ColorScheme.light(
+                      primary: Colors.pink,
+                      onPrimary: Colors.white,
+                      onSurface: Colors.black)),
+              child: child ?? const Text(''),
+            ));
+    if (newDate == null) {
+      return;
+    }
+    setState(() {
+      _callstrtDate = newDate;
+      String cllsrtdate = DateFormat('dd/MM/yyyy').format(newDate);
+      _callStartDateController.text = cllsrtdate;
+    });
   }
 
   Future<void> pickcallStartDate(BuildContext context) async {
